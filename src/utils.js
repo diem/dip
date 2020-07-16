a5a64b5ba07708728da141a24e4f36c4af25f592
oriooctopus <oliverullman@gmail.com>
2020-07-16 14:34:30 -0600

    Design update coinciding with update of docs site

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,8 @@
-import {OVERFLOW_CONTAINER_CLASS} from './theme/Layout';
+import {OVERFLOW_CONTAINER_CLASS} from '@theme/Layout';
 
 export const toTitleCase = str =>
   str.split(' ').map(w => `${w[0].toUpperCase()}${w.substring(1)}`).join('');
 
-export const scrollToTop = () => 
+export const scrollToTop = () => {
   document.querySelector(`.${OVERFLOW_CONTAINER_CLASS}`).scrollTo(0, 0);
+}
