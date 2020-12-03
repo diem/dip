4dba0677c6fd64bc45a519835b27e5b558b9229d
Oliver Ullman <oliverullman@gmail.com>
2020-12-03 16:29:37 -0700

    fix redirects (#98)
    these were written when we thought the url was temporarily going to be dip.diem.com/lip-2

--- static/scripts/new-site-redirects.js
+++ static/scripts/new-site-redirects.js
@@ -1,5 +1,5 @@
 const base = 'https://dip.diem.com';
 
-let path = window.location.pathname.replace('lips', 'dips');
+let path = window.location.pathname.replace('lip', 'dip');
 
 window.location.href = `${base}${path}`;
